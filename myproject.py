80edab1c36d017a4bd1cc7c66573bcc3a1354af5
iamvigneshwaran <vigneshwarana1511@gmail.com>
2023-01-18 23:48:33 +0530

    if else learn

---
+++ myproject.py
@@ -0,0 +1,14 @@
+import math
+
+
+n=int(input("enter number"))
+c=(math.log2(n))
+print(c)
+
+num = int(input("enter the number :"))
+
+if num%10==0:
+    print("number "+str(num)+"is a multiple of 10 ")
+else:
+    print("number is not multiple of 10")
+
