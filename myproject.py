49440aa84bc273cb8d0adb3601be199bcfff3772
iamvigneshwaran <vigneshwarana1511@gmail.com>
2023-01-19 17:25:48 +0530

    leap year or not

--- myproject.py
+++ myproject.py
@@ -1,6 +1,6 @@
 import math
 
-
+"""
 n=int(input("enter number"))
 c=(math.log2(n))
 print(c)
@@ -13,4 +13,21 @@ else:
     print("number is not multiple of 10")
 
 print("hi "*10)
+"""
 
+print("-" * 50)
+year = int(input("enter the year :"))
+
+if year != 0:
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                print("leap year")
+            else:
+                print("not leap year")
+        else:
+            print("leap year")
+    else:
+        print("not leap year")
+else:
+    print("...please Enter valid year!.. ")
