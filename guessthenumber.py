fa7cb1a5e41e0e8fa5d7eb929cbc8907f5475f7e
iamvigneshwaran <vigneshwarana1511@gmail.com>
2023-01-21 00:18:57 +0530

    guess the number program and set max attempt

--- guessthenumber.py
+++ guessthenumber.py
@@ -4,10 +4,21 @@ num = random.randint(1, 20)
 
 guess = int(input("guess the number the number range 1 to 20 "))
 
-while num != guess:
-    if guess > num:
-        print("your guess is grater..")
+attempt = 1
+while True :
+    if num != guess:
+        if attempt != 4:
+            if guess > num:
+                print("your guess is grater..")
+            else:
+                print("your guess is lower...")
+            attempt = attempt + 1
+            guess = int(input("try again..."))
+        else:
+            print("maximam attempt reached... you lost")
+            break
     else:
-        print("your guess is lower...")
-    guess = int(input("try again..."))
-print("you won")
+        print("you won")
+        break
+
+
