d9a0f51474e38024162d844282dc83a34c6c0230
iamvigneshwaran <vigneshwarana1511@gmail.com>
2023-01-23 23:35:58 +0530

    file read write append

---
+++ filereadwriteappend.py
@@ -0,0 +1,19 @@
+'''
+file = open("E:\pythonproject\india","a")
+
+
+
+
+file.write("surya - batsman\n")
+file.write("jadeja - batsman\n")
+
+
+file.close()
+'''
+
+bow = open("bowler.txt", "a")
+
+for player in range(1, 11):
+    i = input("enter your player list :")
+
+bow.close()
