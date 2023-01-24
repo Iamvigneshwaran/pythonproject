9b7daaed5bea8ac5f1cc8a4dbbed2c2278061fce
iamvigneshwaran <vigneshwarana1511@gmail.com>
2023-01-24 23:47:15 +0530

    date time updated and formation

--- datetime.py
+++ datetime.py
@@ -1,5 +1,6 @@
 import time
-time_now=time.strftime("%H:%M:%S",time.localtime())
+
+time_now = time.strftime("%H:%M:%S", time.localtime())
 print(time_now)
-date_now=time.strftime("%Y-%b-%d",time.localtime())
+date_now = time.strftime("%Y-%b-%d", time.localtime())
 print(date_now)
